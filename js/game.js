8a824df7c8d19ffc217bfc92a87840a1c1c6ce78
Taiyo Moriguchi <tmor0018@uni.sydney.edu.au>
2026-05-24 17:10:59 +1000

    fixed assets and changed bkgrd for game

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 let homeScene = new Phaser.Scene('home');
 
 homeScene.preload = function() {
-    this.load.image('bkg', 'assets/bkgfull.png');
+    this.load.image('bkg', 'assets/fullbackground.png');
     this.load.image('b0', 'assets/block0-dialSheet.png');
     this.load.image('b1', 'assets/block1-dialSheet.png');
     this.load.image('b2', 'assets/block2-dialSheet.png');
@@ -14,6 +14,7 @@ homeScene.create = function() {
 
     // Background
     const bkg = this.add.image(W / 2, H / 2, 'bkg');
+    bkg.setOrigin(0.5, 0.5);
     const scaleX = W / bkg.width;
     const scaleY = H / bkg.height;
     bkg.setScale(Math.max(scaleX, scaleY));
@@ -22,31 +23,31 @@ homeScene.create = function() {
     this.add.text(W / 2, 60, 'Resident Evil 2 Block Puzzle', {
         fontFamily: 'Georgia, serif',
         fontSize: '38px',
-        fill: '#c8b97a',
+        fill: '#0cbee1',
         stroke: '#000000',
         strokeThickness: 8,
     }).setOrigin(0.5);
 
     // Layout
     const keys   = ['b0', 'b1', 'b2', 'b3'];
-    const blockW = 220;
+    const blockW = 300;
     const gap    = -40;
     const totalW = blockW * 4 + gap * 3;
     const startX = (W - totalW) / 2;
-    const blockY = 241;
+    const blockY = 220;
 
     const offsets = [
-        { x: 18, y: -3 },  // block 0
-        { x: 8, y: -8 },  // block 1
-        { x: -35, y: -1.5 },  // block 2
-        { x: -36, y: -2 },  // block 3
+        { x: 61, y: 8},  // block 0
+        { x: 28, y: -2 },  // block 1
+        { x: -44, y: 8 },  // block 2
+        { x: -63, y: 6 },  // block 3
     ];
 
     keys.forEach((key, i) => {
         const src  = this.textures.get(key).getSourceImage();
         const rotH = src.height / 4;
         const scale = blockW / src.width;
-        const cropH = i === 0 ? rotH - 18 : rotH;
+        const cropH = i === 0 ? rotH - 11 : i === 1 ? rotH - 1 : rotH;
 
         this.add.image(startX + i * (blockW + gap) + offsets[i].x, blockY + offsets[i].y, key)
             .setOrigin(0, 0)
